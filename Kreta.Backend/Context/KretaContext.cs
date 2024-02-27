202a1f7bf4fe47fe4a9531bf87b0059eee53b657
Gyuris Csaba <gycsaba72@gmail.com>
2024-02-27 14:05:34 +0100

    Initial commit

---
+++ Kreta.Backend/Context/KretaContext.cs
@@ -0,0 +1,17 @@
+﻿using Kreta.Shared.Models;
+using Kreta.Shared.Models.SchoolCitizens;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kreta.Backend.Context
+{
+    public class KretaContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<EducationLevel> EducationLevels { get; set; }
+        public KretaContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Kreta.Desktop/ViewModels/SchoolCitizens/StudentViewModel.cs
@@ -0,0 +1,98 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Kreta.HttpService.Services;
+using Kreta.Shared.Responses;
+using Kreta.Desktop.ViewModels.Base;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Kreta.Shared.Models.SchoolCitizens;
+using Kreta.Shared.Models;
+using System.Linq;
+
+namespace Kreta.Desktop.ViewModels.SchoolCitizens
+{
+    public partial class StudentViewModel : BaseViewModel
+    {        
+        private readonly IStudentService? _studentService;
+        private readonly IEducationLevelService? _educationLevelService;
+
+        [ObservableProperty]
+        private ObservableCollection<Student> _students = new();
+
+        [ObservableProperty]
+        private ObservableCollection<string> _educationLevels = new();
+
+        [ObservableProperty]
+        private Student _selectedStudent;
+      
+        public StudentViewModel()
+        {
+            _selectedStudent = new Student();
+        }
+
+        public StudentViewModel(IStudentService? studentService, IEducationLevelService? educationLevelService)
+        {
+            _selectedStudent = new Student();
+            _studentService = studentService;
+            _educationLevelService = educationLevelService;
+        }
+
+        public async override Task InitializeAsync()
+        {
+            await UpdateView();
+        }
+
+        [RelayCommand]
+        private async Task DoSave(Student newStudent)
+        {
+            if (_studentService is not null)
+            {
+                ControllerResponse result;
+                if (newStudent.HasId)
+                    result = await _studentService.UpdateAsync(newStudent);
+                else
+                    result = await _studentService.InsertAsync(newStudent);
+
+                if (!result.HasError)
+                {
+                    await UpdateView();
+                }
+            }
+        }
+
+        [RelayCommand]
+        private async Task DoRemove(Student studentToDelete)
+        {
+            if (_studentService is not null)
+            {
+                ControllerResponse result = await _studentService.DeleteAsync(studentToDelete.Id);
+                if (result.IsSuccess)
+                {
+                    await UpdateView();
+                }
+            }
+        }
+
+        [RelayCommand]
+        private void DoNewStudent()
+        {
+            SelectedStudent = new Student();
+        }
+
+        private async Task UpdateView()
+        {
+            if (_studentService is not null)
+            {
+                List<Student> students = await _studentService.SelectAllAsync();
+                Students = new ObservableCollection<Student>(students);
+            }
+            if (_educationLevelService is not null)
+            { 
+                List<EducationLevel> educationLevels = await _educationLevelService.SelectAllAsync();
+                EducationLevels = new ObservableCollection<string>(educationLevels.Select(educationLevels => educationLevels.StudentEducationLevel));
+            }
+        }
+
+    }
+}
